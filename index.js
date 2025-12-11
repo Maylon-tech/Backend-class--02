e3890b5fed48f9e06ba7a5a60f78417d88c707e9
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-11 19:42:57 +0900

    initialize CRUD backend class

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+
+app.listen(5000, () => console.log("Listening on port 5000"))
+
+app.get("/", (req, res) => {
+    res.send("Hello World again on backend site...")
+})
