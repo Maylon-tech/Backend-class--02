90b77c91d4f2d9e940e9a654509d2d40bcb714ce
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-15 15:58:11 +0900

    add more dependency mongoose and dotenv

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
+
+
+mongoose.connect("mongodb+srv://minatokiroisen77_db_user:XdObVDt2AoJ6VVUg@crud-api-backend.surgcqc.mongodb.net/?appName=crud-api-backend")
+
 
 app.listen(5000, () => console.log("Listening on port 5000"))
 
